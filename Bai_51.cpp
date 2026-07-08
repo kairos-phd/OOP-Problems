5136e0fbf4dd2d49bc63696c694b499fad5c86d1
Dang Pham <86720875+kairos-phd@users.noreply.github.com>
2026-07-08 21:38:04 +0700

    Update Bai_51.cpp

--- Bai_51.cpp
+++ Bai_51.cpp
@@ -1,5 +1,146 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdlib>
+
 using namespace std;
-class PHANSO{protected:int tuSo,mauSo;public:PHANSO(){tuSo=0;mauSo=1;}void nhap(){cout<<"Nhap tu so: ";cin>>tuSo;cout<<"Nhap mau so: ";cin>>mauSo;if(mauSo==0)mauSo=1;}void xuat(){cout<<tuSo<<"/"<<mauSo;}};
-class HONSO:public PHANSO{private:int phanNguyen;double giaTri(){return phanNguyen+(double)tuSo/mauSo;}public:HONSO():PHANSO(){phanNguyen=0;}void nhap(){cout<<"Nhap phan nguyen: ";cin>>phanNguyen;PHANSO::nhap();}void xuat(){cout<<phanNguyen<<" ";PHANSO::xuat();cout<<endl;}HONSO operator++(){phanNguyen++;return *this;}bool operator>(HONSO a){return giaTri()>a.giaTri();}};
-int main(){HONSO a,b;cout<<"Nhap hon so a"<<endl;a.nhap();cout<<"\nNhap hon so b"<<endl;b.nhap();++b;cout<<"\nHon so b sau khi tang 1 phan nguyen: ";b.xuat();if(a>b)cout<<"a lon hon b sau khi tang"<<endl;else cout<<"a khong lon hon b sau khi tang"<<endl;return 0;}
+
+int ucln(int a, int b)
+{
+    a = abs(a);
+    b = abs(b);
+
+    while (b != 0)
+    {
+        int r = a % b;
+        a = b;
+        b = r;
+    }
+
+    return a;
+}
+
+class PHANSO
+{
+protected:
+    int tu;
+    int mau;
+
+public:
+    PHANSO()
+    {
+        tu = 0;
+        mau = 1;
+    }
+
+    void nhap()
+    {
+        cout << "Nhap tu so: ";
+        cin >> tu;
+
+        do
+        {
+            cout << "Nhap mau so: ";
+            cin >> mau;
+
+            if (mau == 0)
+            {
+                cout << "Mau so phai khac 0. Nhap lai!" << endl;
+            }
+        }
+        while (mau == 0);
+    }
+
+    void xuat()
+    {
+        cout << tu << "/" << mau;
+    }
+
+    void rutGon()
+    {
+        int u = ucln(tu, mau);
+
+        tu = tu / u;
+        mau = mau / u;
+
+        if (mau < 0)
+        {
+            tu = -tu;
+            mau = -mau;
+        }
+    }
+};
+
+class HONSO : public PHANSO
+{
+private:
+    int phanNguyen;
+
+public:
+    HONSO()
+    {
+        phanNguyen = 0;
+        tu = 0;
+        mau = 1;
+    }
+
+    void nhap()
+    {
+        cout << "Nhap phan nguyen: ";
+        cin >> phanNguyen;
+
+        PHANSO::nhap();
+    }
+
+    void xuat()
+    {
+        cout << phanNguyen << " ";
+        PHANSO::xuat();
+    }
+
+    int doiThanhTuSo()
+    {
+        return phanNguyen * mau + tu;
+    }
+
+    HONSO operator ++ ()
+    {
+        phanNguyen++;
+        return *this;
+    }
+
+    bool operator > (HONSO b)
+    {
+        int tu1 = doiThanhTuSo();
+        int tu2 = b.doiThanhTuSo();
+
+        return tu1 * b.mau > tu2 * mau;
+    }
+};
+
+int main()
+{
+    HONSO a, b;
+
+    cout << "Nhap hon so a:" << endl;
+    a.nhap();
+
+    cout << "\nNhap hon so b:" << endl;
+    b.nhap();
+
+    ++b;
+
+    cout << "\nSau khi tang b len 1 phan nguyen, b = ";
+    b.xuat();
+
+    cout << endl;
+
+    if (a > b)
+    {
+        cout << "a lon hon b sau khi tang" << endl;
+    }
+    else
+    {
+        cout << "a khong lon hon b sau khi tang" << endl;
+    }
+
+    return 0;
+}
